7ce74c61efe0e73b0fb5d9760d5c6dbc0a203b8e
zhnuraa <250822@astanait.edu.kz>
2026-02-04 01:24:08 +0500

    Update InvalidInputException.java

--- src/exception/InvalidInputException.java
+++ src/exception/InvalidInputException.java
@@ -1,1 +1,12 @@
+package exception;
+public class InvalidInputException extends IllegalArgumentException {
+
+    public InvalidInputException(String message) {
+        super(message);
+    }
+
+    public InvalidInputException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
 
