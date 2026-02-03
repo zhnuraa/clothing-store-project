89c46053aca3d9b97f141a3562e851c563acb787
zhnuraa <250822@astanait.edu.kz>
2026-02-04 01:44:13 +0500

    Simplify InvalidInputException by removing constructor
    Removed the constructor that accepts a Throwable cause.

--- src/exception/InvalidInputException.java
+++ src/exception/InvalidInputException.java
@@ -1,12 +1,7 @@
 package exception;
-public class InvalidInputException extends IllegalArgumentException {
 
+public class InvalidInputException extends IllegalArgumentException {
     public InvalidInputException(String message) {
         super(message);
     }
-
-    public InvalidInputException(String message, Throwable cause) {
-        super(message, cause);
-    }
 }
-
