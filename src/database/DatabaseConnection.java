5b42ae5084e3ef0b09f81e2a16fe34400c097bbd
zhnuraa <250822@astanait.edu.kz>
2026-02-04 19:13:57 +0500

    Update DatabaseConnection.java

--- src/database/DatabaseConnection.java
+++ src/database/DatabaseConnection.java
@@ -6,20 +6,21 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
 
-private static final String URL = "jdbc:postgresql://localhost:5432/clothing_db";
-private static final String USER = "clothing_user";
-private static final String PASSWORD = "1234";
+    private static final String URL = "jdbc:postgresql://localhost:5432/clothing_db";
+    private static final String USER = "clothing_user";
+    private static final String PASSWORD = "1234";
 
     public static Connection getConnection() {
         try {
-            // Ensure driver is loaded (helps if classpath is weird)
+            // Helps if classpath is weird
             Class.forName("org.postgresql.Driver");
-
             return DriverManager.getConnection(URL, USER, PASSWORD);
+
         } catch (ClassNotFoundException e) {
             System.out.println("PostgreSQL JDBC Driver not found. Add postgresql JDBC jar/dependency.");
             e.printStackTrace();
             return null;
+
         } catch (SQLException e) {
             System.out.println("Connection failed!");
             e.printStackTrace();
